5856c05f99e5105cb30a7437d7294c6657db9cfa
Tom <thomas.desert@gmail.com>
2014-12-06 12:58:43 +0100

    Add source maps to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build', function() {
   gulp.src('src/*.coffee')
+    .pipe(sourcemaps.init())
     .pipe(coffee({ bare: true }).on('error', gutil.log))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'))
 });
 
